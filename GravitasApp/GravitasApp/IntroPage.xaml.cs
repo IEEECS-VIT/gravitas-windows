ae3d7f05eb02abb303616e89815ccd43d243d5f5
VinayGupta23 <vinay.23.gupta@outlook.com>
2015-06-02 21:50:58 +0530

    Added blank page templates

---
+++ GravitasApp/GravitasApp/IntroPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using GravitasApp.Managers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace GravitasApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class IntroPage : Page, IManageable
+    {
+        public IntroPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            PageManager.RegisterPage(this);
+        }
+
+        public Dictionary<string, object> SaveState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadState(Dictionary<string, object> lastState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AllowAppExit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
