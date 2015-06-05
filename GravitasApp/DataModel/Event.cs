73de7a6ddbe470a49587ffc47924e8ab9ada4e5b
sukriti27 <sukriti477@gmail.com>
2015-06-05 20:31:24 +0530

    Update Event.cs

--- GravitasApp/DataModel/Event.cs
+++ GravitasApp/DataModel/Event.cs
@@ -7,5 +7,19 @@ namespace DataModel
 {
     public class Event
     {
+            public string name { get; set; }
+            public string category { get; set; }
+            public string coordinator1Name { get; set; }
+            public string coordinator2Name { get; set; }
+            public long coordinator1Phone { get; set; }
+            public long coordinator2Phone { get; set; }
+            public string description { get; set; }
+            public int fees { get; set; }
+            public string venue { get; set; }
+            public int prize1 { get; set; }
+            public int prize2 { get; set; }
+            public int prize3 { get; set; }
+            public string duration { get; set; }
+            public string miscDetails { get; set; }
     }
 }
