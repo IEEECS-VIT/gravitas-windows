1b463266992c551a271691f2eafd337515346a7f
VinayGupta23 <vinay.23.gupta@outlook.com>
2015-07-06 23:01:22 +0530

    Updated category metadata

--- GravitasApp/GravitasApp/Helpers/CategoryMetadata.cs
+++ GravitasApp/GravitasApp/Helpers/CategoryMetadata.cs
@@ -65,21 +65,56 @@ namespace GravitasApp.Helpers
             get { return _readonlyInfoList; }
         }
 
-        // Ponder: Move data to a Json file (?)
         public static void Initialize()
         {
             _infoList.Add(new CategoryMetadata(
+                "PREMIUM",
+                "Go big. Participate in these massively popular events and take a shot at beating the crowd!",
+                Colors.DarkSalmon));
+            _infoList.Add(new CategoryMetadata(
+                "ROBOMANIA",
+                "Robots are always fascinating! Especially when they can run, drive, jump, fight - everything you wanted to be.",
+                Colors.LightGray));
+            _infoList.Add(new CategoryMetadata(
                 "BITS AND BYTES",
                 "Geek is the new cool! Compete across various domains covering algorithms, networking and more.",
-                Colors.SteelBlue));
+                Colors.LightBlue));
+            _infoList.Add(new CategoryMetadata(
+                "APPLIED ENGINEERING",
+                "Apply everything you've learnt in engineering (no, we're serious) and compete to the teeth.",
+                Colors.Goldenrod));
+            _infoList.Add(new CategoryMetadata(
+                "MANAGEMENT",
+                "Management isn't for the faint hearted. Showcase your leadership and managing skills in these set of events.",
+                Colors.Bisque));
+            _infoList.Add(new CategoryMetadata(
+                "INFORMALS",
+                "Put down your books, have some fun and forget engineering!",
+                Colors.MediumPurple));
+            _infoList.Add(new CategoryMetadata(
+                "BUILTRIX",
+                "Build, plan or construct - this is your platform to show your creativity and skill.",
+                Colors.CornflowerBlue));
+            _infoList.Add(new CategoryMetadata(
+                "CIRCUITRIX",
+                "Let go of your resistance and charge up with these electrifying events!",
+                Colors.MediumSeaGreen));
+            _infoList.Add(new CategoryMetadata(
+                "QUIZ",
+                "Set your hands on the buzzer, its quiz time!",
+                new Color() { R = 200, G = 50, B = 50, A = 255 }));
+            _infoList.Add(new CategoryMetadata(
+                "ONLINE",
+                "With internet being the world's obsession, take part now in a variety of online events.",
+                Colors.Tomato));
             _infoList.Add(new CategoryMetadata(
                 "BIOXYN",
                 "Its the survival of the fittest, time to put those little neurons to use!",
                 Colors.Green));
             _infoList.Add(new CategoryMetadata(
-                "ROBOMANIA",
-                "",
-                Colors.Gray));
+                "SCHOOL",
+                "Some mixed bag and classic events to bring back good old days.",
+                Colors.LightSlateGray));
         }
 
         public static CategoryMetadata GetMetadata(string categoryName)
