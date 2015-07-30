6267c3f65b2efb66c72a008943c9a61d54c2d8b8
VinayGupta23 <vinay.23.gupta@outlook.com>
2015-07-30 16:19:11 +0530

    Added network service class and json parser template, other changes

---
+++ GravitasApp/GravitasSDK/Providers/JsonParser.cs
@@ -0,0 +1,31 @@
+﻿using GravitasSDK.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Windows.Data.Json;
+
+
+namespace GravitasSDK.Providers
+{
+    public static class JsonParser
+    {
+        internal static StatusCode GetStatus(string content)
+        {
+            JsonObject obj = JsonObject.Parse(content);
+            int code = (int)obj.GetNamedNumber("status");
+            switch (code)
+            {
+                case 1: return StatusCode.Success;
+                case 2: return StatusCode.NoChange;
+                default: return StatusCode.ServerError;
+            }
+        }
+
+        public List<Event> GetEvents(string eventsJson)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
